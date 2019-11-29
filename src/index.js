6be7a025eead3f27d21a8f1ac267f19635c3d861
joshcv2112 <joshcv2112@gmail.com>
2019-11-28 20:11:40 -0700

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render() {
+    return (
+      <button className='square' onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  getDivList() {
+    var divList = [];
+
+    for (var i = 0; i < 30; i++) {
+      divList.push(<Square />);
+    }
+
+    return divList;
+  }
+
+  getOtherList() {
+    var otherListThing = [];
+
+    for (var i = 0; i < 16; i++) {
+      otherListThing.push(<div className='board-row'>{this.getDivList()}</div>);
+    }
+
+    return otherListThing;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className='status'>{status}</div>
+        {/* <div className='board-row'>{this.getDivList()}</div> */}
+        {this.getOtherList()}
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className='game'>
+        <div className='game-board'>
+          <Board />
+        </div>
+        <div className='game-info'>
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById('root'));
